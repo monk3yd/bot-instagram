4852a060dff27a7837e60aa6b93c91e7aa46ab47
monk3yd <jegajardog@gmail.com>
2022-09-18 02:09:07 -0300

    Add proof of work and login steps, untested yet

--- main.py
+++ main.py
@@ -13,17 +13,29 @@ def main():
 
 
 def run(playwright):
+    # Variables
     START_URL = "https://www.instagram.com/" 
 
+    # Init browser
     firefox = playwright.firefox
     browser = firefox.launch(headless=False)
 
+    # Config
     context = browser.new_context(
         viewport={'width': 1920, 'height': 1080}
     )
 
     page = context.new_page()
 
+    # Login to instagram
+    page.goto(START_URL)
+    page.locator("input[name=username]").type("username_test", delay=100)
+    page.locator("input[name=password]").type("password_test", delay=100)
+    page.locator("button[type=submit]", has_text="Log In").click()
+
+    # --- Proof of Work ---
+    page.wait_for_timeout(3000)
+    page.screenshot(path="proof.png")
 
 if __name__ == "__main__":
     main()
