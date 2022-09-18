dce04a8664df31e69d5cf0c480512c238be50faf
monk3yd <jegajardog@gmail.com>
2022-09-18 02:23:46 -0300

    Add env variables for username and password

--- main.py
+++ main.py
@@ -1,18 +1,18 @@
+import os
 from playwright.sync_api import sync_playwright
 
 
 def main():
-
-    USERNAME = ""
-    PASSWORD = ""
+    USERNAME = os.getenv("INSTAGRAM_USERNAME")
+    PASSWORD = os.getenv("INSTAGRAM_PASSWORD")
 
     with sync_playwright() as playwright:
-        run(playwright)
+        run(playwright, USERNAME, PASSWORD)
 
     ...
 
 
-def run(playwright):
+def run(playwright, username, password):
     # Variables
     START_URL = "https://www.instagram.com/" 
 
@@ -29,13 +29,17 @@ def run(playwright):
 
     # Login to instagram
     page.goto(START_URL)
-    page.locator("input[name=username]").type("username_test", delay=100)
-    page.locator("input[name=password]").type("password_test", delay=100)
+    page.locator("input[name=username]").type(username, delay=100)
+    page.locator("input[name=password]").type(password, delay=100)
     page.locator("button[type=submit]", has_text="Log In").click()
 
+    # Manage popup
+    page.locator("button[]")
+
     # --- Proof of Work ---
-    page.wait_for_timeout(3000)
+    page.wait_for_timeout(30000)
     page.screenshot(path="proof.png")
+
 
 if __name__ == "__main__":
     main()
