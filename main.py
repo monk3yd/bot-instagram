d5b6a98664f5ef74f6ef23d1ef4498eafeeacf5d
monk3yd <jegajardog@gmail.com>
2022-09-18 03:45:46 -0300

    Add comments and search bar first result of inputted target acc username

--- main.py
+++ main.py
@@ -4,23 +4,33 @@ from playwright.sync_api import sync_playwright
 
 def main():
     '''
-    This bot iterate through another account followers, following them.
+    Instagram Bot.
+    This bot logs in into a desired account given a username and password as,
+    env variables.
+    It searches for target account by username.
+    Then iterate through target account followers, following them.
+
     Input:
         1. username and password of own account as env variables,
         2. target account username as str
+
+    Output:
     '''
+
     # env variables
     USERNAME = os.getenv("INSTAGRAM_USERNAME")
     PASSWORD = os.getenv("INSTAGRAM_PASSWORD")
+
+    TARGET_ACC = "derecho_facil"
     
     # start bot
     with sync_playwright() as playwright:
-        run(playwright, USERNAME, PASSWORD)
+        run(playwright, USERNAME, PASSWORD, TARGET_ACC)
 
     ...
 
 
-def run(playwright, username, password):
+def run(playwright, username, password, target_acc):
     # Variables
     START_URL = "https://www.instagram.com/" 
 
@@ -47,7 +57,10 @@ def run(playwright, username, password):
     # Manage popup notifications
     page.locator("button[class='_a9-- _a9_1']", has_text="Not Now").click()
 
-    #
+    # Search bar
+    page.locator("input[placeholder=Search]").type(target_acc)
+    page.locator("div._ad8j a").first.click()
+    
 
     # --- Proof of Work ---
     page.wait_for_timeout(300000)
