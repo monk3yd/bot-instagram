89318ca2a8b65eda3e7af836a7f3fa1a92a03fde
monk3yd <jegajardog@gmail.com>
2022-09-21 16:42:07 -0300

    Working version, scroll & follow actions done, add timer

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 import os
+import time
 import random
 from playwright.sync_api import sync_playwright
 
@@ -6,7 +7,7 @@ from playwright.sync_api import sync_playwright
 def main():
     '''
     Instagram Bot.
-    This bot logs in into a desired account given a username and password as,
+    This bot logs in into a desired account given a username and password as
     env variables.
     It searches for target account by username.
     Then iterate through target account followers, following them.
@@ -68,40 +69,48 @@ def run(playwright, username, password, target_acc):
     # wait for list followers to load
     # page.wait_for_load_state("networkidle")
 
-    # focus popup window
+    # popup window
     popup_window = page.locator("._aano")
     popup_window.focus()
 
-    # scroll down popup
-    # popup_window.page.mouse.wheel(0, 1000)
-
     arguments_handle = popup_window.evaluate("document.getElementsByClassName('_aano')[0];")
-    last_height = popup_window.evaluate("(arguments) => arguments.scrollHeight;", arguments_handle)
-    # last_height = popup_window.evaluate("document.getElementsByClassName('_aano')[0].scrollHeight;")
-    print(last_height)
+    last_height = popup_window.evaluate("arguments => arguments.scrollHeight;", arguments_handle)
+    print(f"Init height: {last_height}")
 
-    # while True:
-    #     page.keyboard.press("End")
-    #     page.wait_for_timeout(2000)
-    #     new_height = page.evaluate("() => document.body.scrollHeight;")
-    #     print(new_height)
+    while True:
+        # scroll
+        popup_window.evaluate("arguments => arguments.scrollTop = arguments.scrollTop + arguments.scrollHeight;", arguments_handle)
+        
+        # wait for dynamic load (TODO remove hardcode sleep) 
+        page.wait_for_timeout(4000)
+
+        new_height = popup_window.evaluate("arguments => arguments.scrollTop + arguments.scrollHeight;", arguments_handle)
+
+        print(f"\nLast Height: {last_height}")
+        print(f"New Height: {new_height}")
+
+        if new_height == last_height:
+            break
+        last_height = new_height
 
     # Iterate through each follower
-    # followers = page.query_selector_all("._aacl._aaco._aacw._aad6._aade")
-    # for follower in followers:
-    #     follow_status = follower.text_content()
-    #     print(follow_status)
-    #     if "Follow" == follow_status:
-    #         follower.click()
-    #     elif "Following" == follow_status or "Requested" == follow_status:
-    #         continue
-    #     # Add random sleep time between each follow
-    #     page.wait_for_timeout(random.uniform(0.7, 2))
+    followers = page.query_selector_all("._aacl._aaco._aacw._aad6._aade")
+    for follower in followers:
+        follow_status = follower.text_content()
+        print(follow_status)
+        if "Follow" == follow_status:
+            follower.click()
+
+            # Add random sleep time between each follow
+            page.wait_for_timeout(random.uniform(0.7, 2))
 
     # --- Proof of Work ---
-    page.wait_for_timeout(300000)
-    page.screenshot(path="proof.png")
+    # page.wait_for_timeout(300000)
+    # page.screenshot(path="proof.png")
 
 
 if __name__ == "__main__":
+    start = time.perf_counter()
     main()
+    finish = time.perf_counter()
+    print(f'Finished in {round(finish-start, 2)} second(s)') 
